cb44cc653af5699328a053fd28e06b5f81ab7778
bdsondag <47571995+bdsondag@users.noreply.github.com>
2020-10-07 06:15:34 -0400

    added code to functions

--- cse360assignment/src/cse360assignment02/AddingMachine.java
+++ cse360assignment/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,33 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String change;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    change = "0"; // used as way of creating string for toString method
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+	  total = total + value; // adds value to total 
+	  change = change + " + " + value; // adds addition to change string
   }
 
   public void subtract (int value) {
+	  total = total - value; // subtracts value from total 
+	  change = change + " - " + value; // adds subtraction to change string
   }
 
   public String toString () {
-    return "";
+	  return "change"; // return change string which has all operations done to total 
   }
 
   public void clear() {
+	  total = 0; // reset total value
+	  change = "0"; // reset change value
   }
 }
